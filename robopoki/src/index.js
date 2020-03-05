57585e2d0fee804d36cc3ad61fb4880bcca38ce0
Paige Andrews <pandrews25@yahoo.com>
2020-03-05 15:09:26 -0500

    playing with react

--- robopoki/src/index.js
+++ robopoki/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import CarList from './CardList'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import {robots} from './robots';
 
-ReactDOM.render(
-<CarList robots={robots}/>
-, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
